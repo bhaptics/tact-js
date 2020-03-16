8389d165c42c66dd78360236a4d8a772b30ced15
Sangwon Choi <sangwon.choi@bhaptics.com>
2020-03-16 14:25:35 +0900

    Add sample

---
+++ samples/react/precache-manifest.31a625ba27b99be4499c8a40e12f0550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc20769e6ee5874d7bc55fbcedde2ca2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f194e37b92996fe4a8c",
+    "url": "/static/css/main.7136d13a.chunk.css"
+  },
+  {
+    "revision": "a1c892913c9f1ee0b36e",
+    "url": "/static/js/2.0a43607e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0a43607e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f194e37b92996fe4a8c",
+    "url": "/static/js/main.1d000d1e.chunk.js"
+  },
+  {
+    "revision": "88a2c6f3e354bb4c4f63",
+    "url": "/static/js/runtime-main.42c4364f.js"
+  }
+]);
