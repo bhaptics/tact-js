e51fc8486c9ec83ce4ca5fe8f3df6ddbe490adf3
Byeongmin <byeongmin.jeon@bhaptics.com>
2025-03-21 17:21:19 +0900

    feat(demo): add Timer serviceworker

--- demo/src/components/MotorTestSection.tsx
+++ demo/src/components/MotorTestSection.tsx
@@ -1,6 +1,9 @@
 import HapticDriver from 'tact-js';
 
 export default function MotorTestSection() {
+  const ping = async () => {
+    await HapticDriver.pingAll();
+  };
   const testMotors = async () => {
     await HapticDriver.motorTest();
   };
@@ -9,11 +12,18 @@ export default function MotorTestSection() {
     <section className={`flex flex-col items-start gap-2 transition-opacity`}>
       <h3>1. Motor Test</h3>
       <p>If you have a TactSuit connected, you can test the motors by clicking the button below.</p>
-      <button
-        className="cursor-pointer bg-gray-100 p-2 px-4 rounded hover:bg-gray-200 mt-2"
-        onClick={testMotors}>
-        Test
-      </button>
+      <div className="flex gap-2">
+        <button
+          className="cursor-pointer bg-gray-100 p-2 px-4 rounded hover:bg-gray-200 mt-2"
+          onClick={ping}>
+          Ping
+        </button>
+        <button
+          className="cursor-pointer bg-gray-100 p-2 px-4 rounded hover:bg-gray-200 mt-2"
+          onClick={testMotors}>
+          Test
+        </button>
+      </div>
     </section>
   );
 }
