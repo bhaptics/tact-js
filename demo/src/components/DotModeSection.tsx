e59a66e3b08fff0579ccd8d367369a2d081e628f
Byeongmin <byeongmin.jeon@bhaptics.com>
2025-03-24 13:50:15 +0900

    feat(demo): intensity, duration 조절 추가

--- demo/src/components/DotModeSection.tsx
+++ demo/src/components/DotModeSection.tsx
@@ -1,26 +1,54 @@
+import { useState } from 'react';
 import HapticDriver, { PositionType } from 'tact-js';
 
 const motors = Array.from({ length: 40 }).map(() => 0);
 
 export default function DotModeSection() {
+  const [intensity, setIntensity] = useState<number>(100);
+  const [duration, setDuration] = useState<number>(1000);
+
   const handleClick = (index: number) => (front: boolean) => {
     const offset = front ? 0 : 20;
     const newMotors = [...motors];
-    newMotors[index + offset] = 100;
+    newMotors[index + offset] = intensity;
+
     HapticDriver.playDot({
       position: PositionType.Vest,
-      duration: 1000,
+      duration: duration,
       motorValues: newMotors,
     });
   };
 
   return (
-    <section className={`flex flex-col items-start gap-2 transition-opacity`}>
+    <section className={`flex flex-col items-start gap-2 transition-opacity select-none`}>
       <h3>3. Dot Mode Test</h3>
       <p className="">
         {`If you have a TactSuit connected, you can test the motors by clicking the buttons below.
         Each button will trigger a single motor for 1 second.`}
       </p>
+      <div className="flex gap-10">
+        <div className="flex items-center gap-3 text-sm">
+          <label htmlFor="intensity">Intensity (0~100)</label>
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={intensity}
+            onChange={(e) => setIntensity(Number(e.target.value))}
+          />
+        </div>
+        <div className="flex items-center gap-3 text-sm">
+          <label htmlFor="intensity">Duration (ms)</label>
+          <input
+            type="number"
+            min={0}
+            max={200}
+            value={duration}
+            onChange={(e) => setDuration(Number(e.target.value))}
+            className="border border-neutral-200 p-2 rounded w-20"
+          />
+        </div>
+      </div>
       <div className="flex gap-10 w-full justify-center py-5">
         <div className="grid grid-cols-4 gap-2">
           {Array.from({ length: 16 }).map((_, index) => (
--- demo/src/components/PathModeSection.tsx
+++ demo/src/components/PathModeSection.tsx
@@ -1,8 +1,10 @@
 import HapticDriver, { PositionType } from 'tact-js';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { linearScale } from '../utils/Scale';
 
 export default function PathModeSection() {
+  const [intensity, setIntensity] = useState<number>(100);
+  const [duration, setDuration] = useState<number>(100);
   const on = useRef(false);
 
   const handlePointerDown = () => {
@@ -18,16 +20,16 @@ export default function PathModeSection() {
 
     const offset = front ? 0 : 0.5;
     const x = linearScale(e.nativeEvent.offsetX, e.currentTarget.clientWidth, 0) / 2 + offset;
-    const y = linearScale(e.nativeEvent.offsetY, e.currentTarget.clientHeight, 0) / 2 + offset;
+    const y = linearScale(e.nativeEvent.offsetY, e.currentTarget.clientHeight, 0);
 
     console.log(x, y);
 
     HapticDriver.playPath({
       position: PositionType.Vest,
-      duration: 100,
+      duration: duration,
       x: [x],
       y: [y],
-      intensity: [100],
+      intensity: [intensity],
     });
   };
 
@@ -38,6 +40,29 @@ export default function PathModeSection() {
         {`If you have a TactSuit connected, you can test the motors by moving your mouse over the
         area below. The motors will vibrate according to the position of your mouse.`}
       </p>
+      <div className="flex gap-10">
+        <div className="flex items-center gap-3 text-sm">
+          <label htmlFor="intensity">Intensity (0~100)</label>
+          <input
+            type="range"
+            min={0}
+            max={100}
+            value={intensity}
+            onChange={(e) => setIntensity(Number(e.target.value))}
+          />
+        </div>
+        <div className="flex items-center gap-3 text-sm">
+          <label htmlFor="intensity">Duration (ms)</label>
+          <input
+            type="number"
+            min={0}
+            max={200}
+            value={duration}
+            onChange={(e) => setDuration(Number(e.target.value))}
+            className="border border-neutral-200 p-2 rounded w-20"
+          />
+        </div>
+      </div>
       <div className="flex gap-10 w-full justify-center py-5">
         <div
           onPointerDown={handlePointerDown}
